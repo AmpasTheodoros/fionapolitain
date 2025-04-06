0e92d5c2f7491dc3402541905392570b52fd09fd
Theodor Ampas <71594408+AmpasTheodoros@users.noreply.github.com>
2025-04-06 23:27:40 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,12 +15,13 @@ export default {
 			center: true,
 			padding: '2rem',
 			screens: {
-				sm: "640px",
-				md: "768px",
-				lg: "1024px",
-				xl: "1280px",
-				"2xl": "1536px",
-				"3xl": "1720px" 
+				  sm: "640px",
+					md: "768px",
+					lg: "1024px",
+					xl: "1280px",
+					"2xl": "1536px",
+					"3xl": "1720px" 
+			}
 		},
 		extend: {
 			colors: {
